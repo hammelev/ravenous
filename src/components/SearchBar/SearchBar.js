33735e0a1c384ccbb02ba1e3fa018a8abae0f0e1
Hammelev <hammelev@outlook.com>
2025-04-06 20:48:24 +0700

    Added proper options for sorting i.e. best match, highest rated, most reviewed

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -6,12 +6,18 @@ import styles from './searchBar.module.css';
 import SearchReasturant from './SearchRestaurant';
 import SortRestaurantBy from './SortRestaurantBy';
 
+const sortByOptions = {
+    "Best Match": "best_match",
+    "Highest Rated": "rating",
+    "Most Reviewed": "review_count",
+  };
+
 export default function SearchBar(){
 
     return (
         <div className={styles.searchBar} id='searchBar'>
             <SearchReasturant/>
-            <SortRestaurantBy/>
+            <SortRestaurantBy sortByOptions={sortByOptions}/>
         </div>
     );
 }
--- src/components/SearchBar/SortRestaurantBy.js
+++ src/components/SearchBar/SortRestaurantBy.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import styles from './searchBar.module.css';
 
-export default function SortRestaurantBy(){
+export default function SortRestaurantBy({sortByOptions}){
 
     return (
         <form className={styles.sortInput}>
             <label htmlFor='sortRestaurantByOptions'>Sort by:</label>
             <select id='sortRestaurantBy'>
-                <option>Option A</option>
-                <option>Option B</option>
-                <option>Option C</option>
+                {Object.keys(sortByOptions).map(key => <option>{key}</option>)}
             </select>
         </form>
     );
