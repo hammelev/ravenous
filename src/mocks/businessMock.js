eac30ab9dd7f87ca482378e92b9c924f8d1b023b
Hammelev <hammelev@outlook.com>
2025-04-08 16:14:12 +0700

    Added gh-pages to allow easy publishing to Github Pages Refactored the business mock to use a factory function to create mock businesses

--- src/mocks/businessMock.js
+++ src/mocks/businessMock.js
@@ -1,24 +1,37 @@
+const businessFactory = (image, name, city, state, zipcode, category, rating, reviewCount) => {
+    return {
+        image,
+        name,
+        city,
+        state,
+        zipcode,
+        category,
+        rating,
+        reviewCount
+    };
+}
+
 const businessMock = [
-    {
-        image: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-        name: 'Tony\'s pizza',
-        city: 'Almont',
-        state: 'Michigan',
-        zipcode: '48003',
-        category: 'italian',
-        rating: 3.5,
-        reviewCount: 7584
-    },
-    {
-        image: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-        name: 'Domino',
-        city: 'Almont',
-        state: 'Michigan',
-        zipcode: '48003',
-        category: 'italian',
-        rating: 3.2,
-        reviewCount: 5874
-    }
+    businessFactory(
+        'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+        'Tony\'s pizza',
+        'Almont',
+        'Michigan',
+        '48003',
+        'Italian',
+        3.5,
+        7584
+    ),
+    businessFactory(
+        'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+        'Domino',
+        'Almont',
+        'Michigan',
+        '48003',
+        'Italian',
+        3.3,
+        5874
+    )
 ];
 
 export default businessMock;
